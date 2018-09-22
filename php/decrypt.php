4fb18d9780a4f80cea27c8f7b79a2e87292cd7ee
Davyd <davidyeemelyanov@gmail.com>
2018-09-22 23:38:21 +0300

    first

--- php/decrypt.php
+++ php/decrypt.php
@@ -4,12 +4,13 @@ define("c", 13);
 if(!empty($_POST['messagetodecrypt']) && !empty($_POST['keys'])) {
     $message = str_split(strtoupper($_POST['messagetodecrypt']));
     $keys = explode(" ", trim($_POST['keys'], " "));
-    foreach ($message as $mes => $val){
-        if(ord($val) < 65 || ord($val) > 90 ) {
+    foreach ($message as $mes => $val) {
+        if(ord($val) < 65 || ord($val) > 90) {
             $decrypt[] .= " ";
-        }else{
-            $decrypt[] .= chr((ord($val) - $keys[$mes] - c)%26 + ord("a"));
-         }
+        }
+        else {
+            $decrypt[] .= chr((ord($val) - $keys[$mes] - c) % 26 + ord("a"));
+        }
     }
     $decrypt = implode($decrypt);
     $resultdecrypt = array('textdecrypt' => $decrypt, 'arr' => $keys);
--- php/encrypt.php
+++ php/encrypt.php
@@ -5,12 +5,13 @@ if(!empty($_POST["messagetoencrypt"])) {
     $message = $_POST["messagetoencrypt"];
     $message = str_split(strtoupper(trim($message, " ")));
     foreach ($message as $mes => $value) {
-        $key = random_int(0,25);
+        $key = random_int(0, 25);
         $keys[] .= $key . " ";
-        if(ord($value) < 65 || ord($value) > 90 ){
+        if(ord($value) < 65 || ord($value) > 90) {
             $crypt[] .= " ";
-        }else{
-            $crypt[] .= chr((ord($value) + $key - c)%26 + ord("a"));
+        }
+        else {
+            $crypt[] .= chr((ord($value) + $key - c) % 26 + ord("a"));
         }
     }
     $crypt = implode($crypt);
