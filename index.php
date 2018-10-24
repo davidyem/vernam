b83051f0440d4eed9dc8890ebe67047d7ce7a94e
Davyd <davidyeemelyanov@gmail.com>
2018-10-24 16:25:46 +0300

    another

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
         <div class="col-lg-6 text-center">
             <? /*= $serialnum !== null ? var_dump($serialnum) : " ";*/ ?>
             <form id="ajax_form2" method="post" action="">
-                <h4>ВВЕДІТЬ ПОВІДОМЛЕННЯ ДЛЯ ДЕШИФРУВАННЯ</h4><br/>
+                <h4>ВВЕДІТЬ ПОВІДОМЛЕННЯ ДЛЯ РОЗШИФРУВАННЯ</h4><br/>
                 <input id="text2" type="text" name="messagetodecrypt"><br/>
                 <input id="key" name="keys" type="hidden" value="">
                 <button id="decrypt" name="submitdecrypt">Розшифрувати</button>
